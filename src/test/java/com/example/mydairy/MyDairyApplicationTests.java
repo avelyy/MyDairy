479f3c757b1258229dec48fc5ee789ab4cb4ad35
ãavelyy <dldkwls3221@naver.com>
2022-02-16 14:51:05 +0900

    Initial commit

---
+++ src/test/java/com/example/mydairy/MyDairyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydairy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDairyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
